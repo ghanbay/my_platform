7f1e39ce23ebeeb8106c95b7fb8cdcd8fd9612b4
Gülay Hanbay <118522164+ghanbay@users.noreply.github.com>
2023-02-15 21:27:42 +0300

    Update yerde_mi.cs

--- MyPlatform/Assets/Script/yerde_mi.cs
+++ MyPlatform/Assets/Script/yerde_mi.cs
@@ -7,31 +7,23 @@ public class yerde_mi : MonoBehaviour
     public LayerMask layer;
     public bool yerde_miyiz;
     public Rigidbody2D rb;
-    public float speed = 8f;
-
-    void Start()
-    {
-        
-    }
-
+    public float speed = 8f;  
     void Update()
     {
         if (player.oyun_basladi_mi==false)
         {
             return;
         }
-        RaycastHit2D carpýyor_mu = Physics2D.Raycast(transform.position,Vector2.down,0.15f,layer);
-        if (carpýyor_mu.collider !=null)
-        {
-            //zemine çarpýyor
+        RaycastHit2D carpÃ½yor_mu = Physics2D.Raycast(transform.position,Vector2.down,0.15f,layer);
+        if (carpÃ½yor_mu.collider !=null)
+        {       
             yerde_miyiz = true;
         }
         else
-        {
-            //havadayýz
+        {           
             yerde_miyiz = false;
         }
-        //klavyeden tuþa baðma input ile yapýlýr. tuþa basma getkeydown ile kullanýlýr.
+     
         if (yerde_miyiz == true && Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, speed);
